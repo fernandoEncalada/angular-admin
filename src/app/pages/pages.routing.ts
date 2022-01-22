4aa815b720d235f3f6578944356ae0e8f241a9d7
Fernando Encalada <ferencalada1@gmail.com>
2022-01-22 00:55:11 -0500

    Config router

---
+++ src/app/pages/pages.routing.ts
@@ -0,0 +1,26 @@
+import { Routes, RouterModule } from "@angular/router";
+import { NgModule } from "@angular/core";
+
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { Graph1Component } from "./graph1/graph1.component";
+import { PagesComponent } from "./pages.component";
+import { ProgressComponent } from "./progress/progress.component";
+
+
+const routes: Routes = [
+    { 
+        path: 'dashboard',
+        component: PagesComponent, 
+        children: [
+          { path: '', component: DashboardComponent },
+          { path: 'progress', component: ProgressComponent },
+          { path: 'graph1', component: Graph1Component },
+        ]
+      },
+];
+
+@NgModule({
+    imports: [ RouterModule.forChild(routes)],
+    exports: [ RouterModule ]
+})
+export class PagesRoutingModule {}
